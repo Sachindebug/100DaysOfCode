45b32924a5441f922632d707ac53d0ea4ca5e193
Sachin <66303835+Sachindebug@users.noreply.github.com>
2021-06-07 17:08:28 +0530

    Create solution.cpp

---
+++ Day2/Product of elements of Array except self/solution.cpp
@@ -0,0 +1,38 @@
+/*
+    Time Complexity : O(N)
+    Space complexity : O(1)
+
+    where N is the size of the input array
+    Does not contain main function
+*/
+
+int multiply(int a, int b)
+{
+    int mod = 1e9 + 7;
+    long long ret = a % mod;
+    ret *= (b % mod);
+    ret = ret % mod;
+    return ret;
+}
+
+int *getProductArrayExceptSelf(int *arr, int n)
+{
+    int *output = new int[n];
+    int product = 1;
+
+    for (int i = 0; i < n; ++i) 
+    {
+        output[i] = product;
+        product = multiply(product, arr[i]);
+    }
+
+    product = 1;
+
+    for (int i = n - 1; i >= 0; --i) 
+    {
+        output[i] = multiply(product, output[i]);
+        product = multiply(product, arr[i]);
+    }
+
+    return output;
+}
