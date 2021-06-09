7274d8806c00166e4b2868609b2d92949a40666b
Sachin <66303835+Sachindebug@users.noreply.github.com>
2021-06-09 20:43:12 +0530

    Create solution.cpp

---
+++ Day4/Reverse words in a string/solution.cpp
@@ -0,0 +1,8 @@
+string reverseWords(string s) {
+        stringstream all(s); 
+        string word,ans = "";
+        while (all >> word)
+            ans = word + " " + ans;
+        return ans.substr(0,ans.length()-1);
+    }
+//solution uses string stream
