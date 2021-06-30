d390661454b3941e8aa922bf8afaaac2c0ef7a68
Sachin <66303835+Sachindebug@users.noreply.github.com>
2021-06-30 10:47:12 +0530

    Create Solution.cpp

---
+++ Day 14/Sort 0,1,2/Solution.cpp
@@ -0,0 +1,27 @@
+void sort012(int arr[], int n)
+    {
+        // coode here 
+        long int l=0,m=0,r=n-1;
+        while(m<=r)
+        {
+        if(arr[m]==0)
+        {
+            swap(arr[l],arr[m]);
+            l++;
+            m++;
+            
+        }
+        else if(arr[m]==1)
+        {
+            m++;
+        }
+        else 
+        {
+            swap(arr[m],arr[r]);
+            
+            r--;
+            
+        }
+    }
+    }
+    
